9f72a7e5df38e925a82a7b2062573f0dc9d3d8fd
João Vieira <mutantinotrue@outlook.com>
2024-04-01 21:38:46 -0300

    Adicionando TAGS, GALERIA E PESQUISA

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import styled from "styled-components";
 import EstilosGlobais from "./componentes/EstilosGlobais/index.jsx";
 import Cabecalho from "./componentes/Cabecalho/index.jsx";
 import BarraLateral from "./componentes/BarraLateral/index.jsx";
+import Banner from "./componentes/Banner/index.jsx";
+import bannerBackground from './assets/banner.png'
+import Galeria from './componentes/Galeria'
+import fotos from './fotos.json'
+import {useState} from "react";
 
 const FundoGradiente = styled.div`
     background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -9,14 +14,44 @@ const FundoGradiente = styled.div`
     min-height: 100vh;
 `
 
-function App() {
+const AppContainer = styled.div`
+    width: 1440px;
+    margin: 0 auto;
+    max-width: 100%;
+`
+
+const MainContainer = styled.main`
+    display: flex;
+    gap: 24px;
+`
+
+const ConteudoDaGaleria = styled.section`
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+`
+
+const App = () => {
+
+    const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
 
     return (
         <>
             <FundoGradiente>
                 <EstilosGlobais/>
-                <Cabecalho/>
-                <BarraLateral/>
+                <AppContainer>
+                    <Cabecalho/>
+                    <MainContainer>
+                        <BarraLateral/>
+                        <ConteudoDaGaleria>
+                            <Banner
+                                texto="A galeria mais completa de fotos do espaço!"
+                                backgroundImage={bannerBackground}
+                            />
+                            <Galeria fotos={fotosDaGaleria}/>
+                        </ConteudoDaGaleria>
+                    </MainContainer>
+                </AppContainer>
             </FundoGradiente>
         </>
     )
---
+++ src/componentes/Galeria/index.jsx
@@ -0,0 +1,43 @@
+import Titulo from "../Titulo/index.jsx";
+import Tags from "./Tags/index.jsx";
+import Populares from "./Populares/index.jsx";
+import styled from "styled-components";
+import React from "react";
+import Imagem from "./Imagem/index.jsx";
+
+const GaleriaContainer = styled.section`
+    display: flex;
+`
+
+const SecaoFluida = styled.section`
+    flex-grow: 1;
+`
+
+const ImagensContainer = styled.section`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 24px;
+`
+
+const Galeria = ({ fotos = [] }) => {
+    return (
+        <>
+            <Tags />
+            <GaleriaContainer>
+                <SecaoFluida>
+                    <Titulo>Navegue pela galeria</Titulo>
+                    <ImagensContainer>
+                        {fotos.map(foto => <Imagem
+                            key={foto.id}
+                            foto={foto} />)
+                        }
+                    </ImagensContainer>
+                </SecaoFluida>
+                <Populares />
+            </GaleriaContainer>
+        </>
+    )
+}
+
+export default Galeria;
